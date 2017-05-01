5b78990a44005e91f4ea74412114a15a570e9369
qianweijian <315905217@qq.com>
2017-05-01 19:29:34 +0800

    上传功能

--- Application/Admin/Controller/BookController.class.php
+++ Application/Admin/Controller/BookController.class.php
@@ -21,4 +21,40 @@ class BookController extends Controller {
       	$this->assign("booklist",$booklist);
         $this->display();	
     }
+    /**
+     * 显示添加商品页面
+     * @return [type] [description]
+     */
+    public function bookadd()
+    {
+        $model=M('menu');
+        $menulist=$model->select();
+        $this->assign("menulist",$menulist);
+        $this->display();
+    }
+    /**
+     * 添加商品,写入数据库 
+     * @return [type] [description]
+     */
+    public function add()
+    {
+
+
+      if(IS_POST){
+        $model=D('book'); //实例化自定义对象
+
+        foreach ($_POST as $key => $value) { 
+            $post[$key]=trim($value);
+        } 
+        if($model->saveData($post,$_FILES['file'])){   
+              $this->success('添加成功');
+              
+        }else{
+            $this->error('添加失败');
+        } 
+      } 
+
+
+
+    }
 }
---
+++ Application/Admin/Model/BookModel.class.php
@@ -0,0 +1,32 @@
+<?php
+namespace Admin\Model;
+use Think\Model;
+class BookModel extends Model{
+	protected $_auto=array(
+			array(
+				"book_up_time",'time',1,'function'
+			) 
+		);  
+	//文件上传
+	public function saveData($post,$file)
+	{ 
+		if(!$file['error']){
+			$cfg=array(
+					"rootPath" => WORKING_PATH . UPLOAD_PATH,  
+				);
+			$upload=new \Think\Upload($cfg);
+			$info = $upload -> uploadOne($file);
+			
+			//判断是否上传成功
+			if($info){
+				//补全字段 
+				$post['img_path'] = UPLOAD_PATH . $info['savepath'] . $info['savename'];
+				$post['book_publish_time'] = strtotime($post['book_publish_time']);
+				 
+			 	$result=$this->add($post);   
+			} 
+		}
+		return $result;
+ 		
+	}
+}
