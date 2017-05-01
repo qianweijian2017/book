d5086070801075c62c33007a47285cc812d84f81
qianweijian <315905217@qq.com>
2017-05-01 23:17:57 +0800

    订单

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -6,23 +6,25 @@ use Think\Verify;
 * 用户类
 */
 class UserController extends Controller
-{
-	
-	public function login()
-	{  
-		$this->display();
-	}
-	public function register()
-	{	
-		$this->display();
-	}
-	public function verify()
+{ 
+	public function myorder()
 	{
+		$user=session("auth");
+		if($user){
+			$model=M('buyed');
+			$buyed=$model->alias("a")
+					 ->join("__USER__ u on u.id=a.user_id")
+					 ->join("__BOOK__ b on b.id=a.book_id")
+					 ->where("a.user_id={$user['id']}")
+					 ->field("order_no,b.id,buyed_time,img_path,book_name,book_prince,book_zk_prince")
+					 ->select();
+			$this->assign("orderlist",$buyed);
+			 
+		}else{
+			$this->error("请先点击右上角登陆");
+		}
 
-		$verify=new Verify();
-		$verify->useCurve=false;	//混淆曲线
-		$verify->useNoise=true;		//添加杂点
-		$verify->entry();			//绘制
-	}
-	 
+		$this->display();
+	} 
+	  
 }
