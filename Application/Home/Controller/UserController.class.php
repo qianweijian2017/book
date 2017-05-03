529ab597fbc78e4820398700e39ee6d624ba6651
qianweijian <315905217@qq.com>
2017-05-03 15:19:49 +0800

    添加购物车遍历

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -7,6 +7,10 @@ use Think\Verify;
 */
 class UserController extends Controller
 { 
+	/**
+	 * 我的订单
+	 * @return [type] [description]
+	 */
 	public function myorder()
 	{
 		$user=session("auth");
@@ -25,5 +29,22 @@ class UserController extends Controller
 
 		$this->display();
 	} 
+	public function myCar()
+	{
+		$auth=session("auth");
+		$model=M('cart');
+		if(!$auth){
+			$this->error('请先登陆');exit;
+		}
+		$cartlist=$model->alias("c")
+						// ->join("__USER__ u on u.id=c.user_id")
+						->join("__BOOK__ b on b.id=c.book_id")
+						->field("c.*,b.*,b.id as book_id,c.id as cart_id")
+						->where("c.user_id = {$auth['user_id']}")
+						->select();
+						// dump($cartlist);exit;
+		$this->assign("cartlist",$cartlist);
+		$this->display();
+	}
 	  
 }
