8afb1a303235b2eb269d2c73f3d9c2f8fb42ead3
qianweijian <315905217@qq.com>
2017-05-04 20:32:48 +0800

    购物列表

--- Application/Admin/Controller/BuyedController.class.php
+++ Application/Admin/Controller/BuyedController.class.php
@@ -9,11 +9,13 @@ class BuyedController extends AuthController {
 		//获取收藏列表
 		$buyed=M('buyed'); 
 		$buyedlist = $buyed 
-					 -> alias("a") 
+					 -> alias("a")
+					 -> join("__USER__ u on u.id = a.user_id") 
 					 -> join("__BOOK__ b on b.id=a.book_id") //关联图书表
-					 -> field("b.*,a.*,b.id as book_id")
+					 -> field("b.*,a.*,u.*,b.id as book_id")
 					 -> select();
 		$this->assign("buyedlist",$buyedlist);
+		 
 		//获取分页
 		$count=$buyed->field($buyed->getPk())->count();  
         $page=new \Org\Util\page($count,8); //count总页数,limit是显示的行数   
