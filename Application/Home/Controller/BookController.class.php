29f9dcad00e74ae4c0ec9f2063b402c735b9d7bf
qianweijian <315905217@qq.com>
2017-05-04 14:01:02 +0800

    添加搜索功能

--- Application/Home/Controller/BookController.class.php
+++ Application/Home/Controller/BookController.class.php
@@ -12,32 +12,55 @@ class BookController extends Controller
 	 * @return [type] [description]
 	 */
 	public function booklist(){
+
 		 $book=M("book"); //创建书本对象
 		 $menu=M("menu");//创建菜单对象
 		 $menulist= $menu->select(); 
-		 if(I('type')){
-		 	 $where='book_type='.I('type');//是否有分类 
-		 } 
-		 $sort="book_publish_time desc";
-		 if(I('sort')!="2") {
-		 	$sort=self::getSort(I('sort'));
-		 } 
-     	 $count=$book->alias("b")->join("__MENU__ c on b.book_type=c.id")->where($where)->count(); //where 为条件,可作分类分页  
-     	 $page=new Page($count,8);	//count总页数,limit是显示的行数   
-  	     $sPages=$page->show();  
+		 if(!I('get.bk_keyword',0)){ 
+			 if(I('type')){
+			 	 $where='book_type='.I('type');//是否有分类 
+			 } 
+			 $sort="book_publish_time desc";
+			 if(I('sort')!="2"){
+			 	$sort=self::getSort(I('sort'));
+			 } 
+	     	 $count=$book->alias("b")
+						     	 ->join("__MENU__ c on b.book_type=c.id")
+						     	 ->where($where)
+						     	 ->count(); //where 为条件,可作分类分页  
+	     	 $page=new Page($count,8);	//count总页数,limit是显示的行数   
+	  	     $sPages=$page->show();  
 
-		 $booklist=$book->alias("b")
-					->field("book_name,type_name,book_author,book_detail,
-							book_collect,book_prince,book_type,book_zk_prince,img_path,b.id")//查询所需要的字段
-					->where($where)	 
-					->join("__MENU__ c on b.book_type=c.id") 		//通过字段关联表
-					->order($sort)
-					->limit($page->firstRow.',8')
-					->select(); 
-					 
+			 $booklist=$book->alias("b")
+						->field("book_name,type_name,book_author,book_detail,
+								book_collect,book_prince,book_type,book_zk_prince,img_path,b.id")//查询所需要的字段
+						->where($where)	 
+						->join("__MENU__ c on b.book_type=c.id") 		//通过字段关联表
+						->order($sort)
+						->limit($page->firstRow.',8')
+						->select(); 
+		  }else{
+		  	//搜索条件
+		  	 $map['book_author|type_name|book_name'] =
+		  	 			array('like',array('%'.I('get.bk_keyword').'%'),'OR');
+		  	 $count   =   $book->where($map)
+				  	 			 ->alias('b')
+				  	 			 ->join('__MENU__ m on m.id=b.book_type ')
+								 ->count(); //where 为条件,可作分类分页  
+	     	 $page    = new Page($count,8);	//count总页数,limit是显示的行数   
+	  	     $sPages  = $page->show();  
+		  	 $booklist= $book->alias('b')
+		  	 				->join('__MENU__ m on m.id=b.book_type')
+		  	 				->where($map)
+		  				    ->field("book_name,type_name,book_author,book_detail,
+								book_collect,book_prince,book_type,book_zk_prince,img_path")
+		  				    ->limit($page ->firstRow.',8')
+						    ->select();  
+		  }
+		 
  		 $this->assign('sPages',$sPages);// 赋值分页
-		 $this->assign("booklist", $booklist);
-  		 $this->assign("menulist",$menulist);	 
+		 $this->assign('booklist',$booklist);
+  		 $this->assign('menulist',$menulist);	 
 		 $this->display();		//展示模板
 	}
 	/**
@@ -50,7 +73,7 @@ class BookController extends Controller
 			$id=I('get.type');
 			$model=M('book');
 			$bookdetail=$model->find($id);  
-			$model->where("id=$id")->setInc("browse",1)    //浏览量+1 
+			$model->where("id=$id")->setInc("browse",1);    //浏览量+1 
 			$this->assign("bookdetail",$bookdetail); 
 			$this->display();
 		}
